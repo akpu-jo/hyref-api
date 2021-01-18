dd14db4a07b5e8e0af8a369b87d113aa805bebaf
Joseph Akpu <akpu.jo@gmail.com>
2021-01-18 09:39:24 +0100

    changed port settings

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const http = require('http');
+
+const port = process.env.PORT || 3000
 
 const app = express();
 
@@ -36,7 +39,8 @@ app.get("/providers", function(req, res){
     });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(port,() => {
+    console.log(`Server running at port `+port);
+  });
+
 
