426184f69bda6feabaa6de8061aee170b75ed051
Joseph Akpu <akpu.jo@gmail.com>
2021-01-28 13:54:37 +0100

    ...

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+var env = require('dotenv').config();
 const http = require('http');
 
 const app = express();
@@ -9,36 +10,66 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://jakpu:Midi@000@hyrefcluster.se4we.mongodb.net/providerDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://" + process.env.COSMOSDB_HOST + ":" + process.env.COSMOSDB_PORT + "/" + process.env.COSMOSDB_DBNAME + "?ssl=true&replicaSet=globaldb", {
+    auth: {
+        user: process.env.COSMOSDB_USER,
+        password: process.env.COSMOSDB_PASSWORD
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    retryWrites: false
+})
+    .then(() => console.log('Connection to CosmosDB successful'))
+    .catch((err) => console.error(err));
 
-const providerSchema = {
+const Provider = mongoose.model("Provider", new mongoose.Schema({
     provider_name: String,
     category: String,
     coverage_type: String,
     phone: String,
-    location: Object,
+    location: {
+        address: String,
+        city: String,
+        state: String
+    },
     email: String,
-    specialist_clinic: Array
-};
+    specialist_clinic: [
+        {
+            specialist_type: String,
+            is_inhouse: Boolean,
+            on_request: Boolean,
+            clinic_days_time: Array,
+        }
+    ]
+}));
 
-const Provider = mongoose.model("Provider", providerSchema);
 
-app.get("/providers", function(req, res){
-    Provider.find(function(err, providerList){
+app.get("/", (req, res) => {
+    res.json("Wellcome to HyRef API, for the list of providers and their specialist clinics, please follow the link https://hyref.azurewebsites.net/providers")
+});
+
+app.get("/providers", function (req, res) {
+    Provider.find(function (err, providerList) {
         if (!err) {
             res.send(providerList)
-        }else{
+        } else {
             res.send(err);
         }
     });
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server running at port 3000");
-  });
+});
 
 
